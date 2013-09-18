13cfc8a4cbc823385e2a5b6942fac46584453582
Maksim Kavaliou <kovalev@targetprocess.com>
2013-09-18 15:19:55 +0300

    initial application

---
+++ JsonSqlExtensions/JsonSqlExtensions.Core/JsonSqlExtensions.Core/JsonModifier.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.SqlServer.Server;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonSqlExtensions.Core
+{
+	public class JsonModifier
+	{
+		[SqlFunction]
+		public static string GetJsonProperty(string jsonString, string propertyFullKey)
+		{
+			JObject jsonObject = JObject.Parse(jsonString);
+
+			var propertiesKeys = propertyFullKey.Split('.').ToList();
+
+			JToken propertyValue = jsonObject[propertiesKeys[0]];
+
+			for (int i = 1; i < propertiesKeys.Count; i++)
+			{
+				propertyValue = propertyValue[propertiesKeys[i]];
+
+				if (propertyValue == null)
+				{
+					return null;
+				}
+			}
+
+			try
+			{
+				return (string)propertyValue;
+			}
+			catch (Exception e)
+			{
+				return null;
+			}
+		}
+	}
+}
---
+++ JsonSqlExtensions/JsonSqlExtensions.Tests/JsonModifierTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using JsonSqlExtensions.Core;
+using NUnit.Framework;
+
+namespace JsonSqlExtensions.Tests
+{
+	[TestFixture]
+	public class JsonModifierTests
+	{
+		[TestCase("{id: \"1\", name: \"maks\", address: {street: \"xsxsxs\", building: \"4\"} }", "id", "1")]
+		[TestCase("{id: \"1\", name: \"maks\", address: {street: \"xsxsxs\", building: \"4\"} }", "name", "maks")]
+		[TestCase("{id: \"1\", name: \"maks\", address: {street: \"xsxsxs\", building: \"4\"} }", "address.building", "4")]
+		[TestCase("{id: \"1\", name: \"maks\", address: {street: \"xsxsxs\", building: \"4\"} }", "address", null)]
+		public void JsonModifier_ChangeProperty_Check(string jsonString, string propertyKey, string propertyAssertValue)
+		{
+			var propertyValue = JsonModifier.GetJsonProperty(jsonString, propertyKey);
+
+			Assert.That(propertyValue, Is.EqualTo(propertyAssertValue));
+		}
+
+		[TestCase(@"d:\Development\SqlExtensions\SqlManagedExtensions\SqlManagedExtensions\bin\Debug\SqlManagedExtensions.dll")]
+		public static void GetHexString(string assemblyPath)
+		{
+			if (!Path.IsPathRooted(assemblyPath))
+				assemblyPath = Path.Combine(Environment.CurrentDirectory, assemblyPath);
+
+			var builder = new StringBuilder();
+			builder.Append("0x");
+
+			using (var stream = new FileStream(assemblyPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+			{
+				int currentByte = stream.ReadByte();
+				while (currentByte > -1)
+				{
+					builder.Append(currentByte.ToString("X2", CultureInfo.InvariantCulture));
+					currentByte = stream.ReadByte();
+				}
+			}
+
+			var result = builder.ToString();
+
+			Assert.That(result, Is.Not.Null);
+		}
+	}
+}

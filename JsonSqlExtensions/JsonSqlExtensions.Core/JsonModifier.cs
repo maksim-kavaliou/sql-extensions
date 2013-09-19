b95e0f6fc590b32f4de82eab78b0232604de4667
Maksim Kavaliou <kovalev@targetprocess.com>
2013-09-19 14:47:08 +0300

    json arrays modifications

--- JsonSqlExtensions/JsonSqlExtensions.Core/JsonModifier.cs
+++ JsonSqlExtensions/JsonSqlExtensions.Core/JsonModifier.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.SqlServer.Server;
 using Newtonsoft.Json;
@@ -28,7 +27,106 @@ namespace JsonSqlExtensions.Core
 				}
 			}
 
-			return propertyValue.ToString();
+			return JTokenToString(propertyValue);
+		}
+
+		[SqlFunction]
+		public static string ModifyCardSettings(string jsonString)
+		{
+			const string impediment = "impediment";
+			
+			JObject jsonObject = JObject.Parse(jsonString);
+
+			JToken impedimentValue = jsonObject[impediment];
+
+			if (impedimentValue != null)
+			{
+				if (impedimentValue is JArray)
+				{
+					ParseJArray(impedimentValue, TransferAssignmentsBig2ToResponsiblePerson);
+				}
+				else
+				{
+					impedimentValue = TransferAssignmentsBig2ToResponsiblePerson(impedimentValue);
+				}
+
+				jsonObject[impediment] = impedimentValue;
+			}
+
+			return JTokenToString(jsonObject);
+		}
+
+		public static string JTokenToString(JToken jToken)
+		{
+			if (jToken == null)
+			{
+				return null;
+			}
+
+			if (jToken is JObject || jToken is JArray)
+			{
+				return jToken.ToString(Formatting.None);
+			}
+
+			// if jToken is JValue or smth else
+			return jToken.ToString();
+		}
+
+		public static JToken ApplyStringToJToken(string content, JToken destination)
+		{
+			if (destination is JObject)
+			{
+				destination = JObject.Parse(content);
+			}
+			else if (destination is JArray)
+			{
+				destination = new JArray(content);
+			}
+			else
+			{
+				// destination is JValue
+				destination = content;
+			}
+
+			return destination;
+		}
+
+		public static void ParseJArray(JToken jToken, Func<JToken, JToken> jTokenHanler)
+		{
+			if (jToken == null)
+			{
+				return;
+			}
+
+			var array = (JArray) jToken;
+
+			for(int i = 0; i < array.Count; i++)
+			{
+				var item = array[i];
+
+				if (item is JArray)
+				{
+					ParseJArray(item, jTokenHanler);
+				}
+				else
+				{
+					array[i] = jTokenHanler(item);
+				}
+			}
+		}
+
+		public static JToken TransferAssignmentsBig2ToResponsiblePerson(JToken jToken)
+		{
+			const string assignmentsBig2 = "assignments_big_2";
+			const string responsiblePerson = "responsible_person";
+
+			var stringValue = JTokenToString(jToken);
+
+			stringValue = stringValue.Replace(assignmentsBig2, responsiblePerson);
+
+			jToken = ApplyStringToJToken(stringValue, jToken);
+
+			return jToken;
 		}
 	}
 }
--- JsonSqlExtensions/JsonSqlExtensions.Tests/JsonModifierTests.cs
+++ JsonSqlExtensions/JsonSqlExtensions.Tests/JsonModifierTests.cs
@@ -10,15 +10,26 @@ namespace JsonSqlExtensions.Tests
 	[TestFixture]
 	public class JsonModifierTests
 	{
-		[TestCase("{id: \"1\", name: \"maks\", address: {street: \"xsxsxs\", building: \"4\"} }", "id", "1")]
-		[TestCase("{id: \"1\", name: \"maks\", address: {street: \"xsxsxs\", building: \"4\"} }", "name", "maks")]
-		[TestCase("{id: \"1\", name: \"maks\", address: {street: \"xsxsxs\", building: \"4\"} }", "address.building", "4")]
-		[TestCase("{id: \"1\", name: \"maks\", address: {street: \"xsxsxs\", building: \"4\"} }", "address", "{street: \"xsxsxs\", building: \"4\"}")]
+		[TestCase("{\"id\": \"1\", \"name\": \"maks\", \"address\": {\"street\": \"xsxsxs\", \"building\": \"4\"} }", "id", "1")]
+		[TestCase("{\"id\": \"1\", \"name\": \"maks\", \"address\": {\"street\": \"xsxsxs\", \"building\": \"4\"} }", "name", "maks")]
+		[TestCase("{\"id\": \"1\", \"name\": \"maks\", \"address\": {\"street\": \"xsxsxs\", \"building\": \"4\"} }", "address.building", "4")]
+		[TestCase("{\"id\": \"1\", \"name\": \"maks\", \"address\": {\"street\": \"xsxsxs\", \"building\": \"4\"} }", "address", "{\"street\":\"xsxsxs\",\"building\":\"4\"}")]
 		public void JsonModifier_ChangeProperty_Check(string jsonString, string propertyKey, string propertyAssertValue)
 		{
 			var propertyValue = JsonModifier.GetJsonProperty(jsonString, propertyKey);
 
 			Assert.That(propertyValue, Is.EqualTo(propertyAssertValue));
+		}
+
+		[TestCase("{\"impediment\":{\"l\":[[{\"id\":\"general_entity_id\",\"alignment\":\"base\"},{\"id\":\"tags_short\",\"alignment\":\"base\"},{\"id\":\"request_private\",\"alignment\":\"base\"},{\"id\":\"project_abbr\",\"alignment\":\"alt\"}],[{\"id\":\"entity_name_3lines\",\"alignment\":\"base\"}],[],[{\"id\":\"owner\",\"alignment\":\"alt\"},{\"id\":\"related_entity_short\",\"alignment\":\"alt\"}],[{\"id\":\"dependencies\",\"alignment\":\"base\"},{\"id\":\"business_value_short\",\"alignment\":\"base\"},{\"id\":\"assignments_big_2\",\"alignment\":\"base\"}]]},\"userstory\":{\"m\":[[{\"id\":\"general_entity_id\",\"alignment\":\"base\"},{\"id\":\"impediments\",\"alignment\":\"alt\"}],[],[{\"id\":\"create_date\",\"alignment\":\"alt\"},{\"id\":\"assignments_big_2\",\"alignment\":\"base\"}]]},\"bug\":{\"m\":[[{\"id\":\"general_entity_id\",\"alignment\":\"base\"},{\"id\":\"tags_short_xs\",\"alignment\":\"base\"},{\"id\":\"impediments\",\"alignment\":\"alt\"}],[],[{\"id\":\"effort_total\",\"alignment\":\"base\"}]]}}", "{\"impediment\":{\"l\":[[{\"id\":\"general_entity_id\",\"alignment\":\"base\"},{\"id\":\"tags_short\",\"alignment\":\"base\"},{\"id\":\"request_private\",\"alignment\":\"base\"},{\"id\":\"project_abbr\",\"alignment\":\"alt\"}],[{\"id\":\"entity_name_3lines\",\"alignment\":\"base\"}],[],[{\"id\":\"owner\",\"alignment\":\"alt\"},{\"id\":\"related_entity_short\",\"alignment\":\"alt\"}],[{\"id\":\"dependencies\",\"alignment\":\"base\"},{\"id\":\"business_value_short\",\"alignment\":\"base\"},{\"id\":\"responsible_person\",\"alignment\":\"base\"}]]},\"userstory\":{\"m\":[[{\"id\":\"general_entity_id\",\"alignment\":\"base\"},{\"id\":\"impediments\",\"alignment\":\"alt\"}],[],[{\"id\":\"create_date\",\"alignment\":\"alt\"},{\"id\":\"assignments_big_2\",\"alignment\":\"base\"}]]},\"bug\":{\"m\":[[{\"id\":\"general_entity_id\",\"alignment\":\"base\"},{\"id\":\"tags_short_xs\",\"alignment\":\"base\"},{\"id\":\"impediments\",\"alignment\":\"alt\"}],[],[{\"id\":\"effort_total\",\"alignment\":\"base\"}]]}}")]
+		[TestCase("{\"impediment\":\"assignments_big_2\"}", "{\"impediment\":\"responsible_person\"}")]
+		[TestCase("{\"impediment\":[[\"11\",\"assignments_big_2\",[\"33\",\"44\"]],{\"id\":\"assignments_big_2\"},{\"id\":\"bla-bla-bla\"}]}", "{\"impediment\":[[\"11\",\"responsible_person\",[\"33\",\"44\"]],{\"id\":\"responsible_person\"},{\"id\":\"bla-bla-bla\"}]}")]
+		[TestCase("{\"impediment\":[]}", "{\"impediment\":[]}")]
+		public void ModifyCardSettingsCheck(string jsonString, string assertValue)
+		{
+			var result = JsonModifier.ModifyCardSettings(jsonString);
+
+			Assert.That(result, Is.EqualTo(assertValue));
 		}
 
 		[TestCase(@"d:\Development\SqlExtensions\sql-extensions\JsonSqlExtensions\JsonSqlExtensions.Core\bin\Debug\JsonSqlExtensions.Core.dll")]
